d57f5e95c2e05c91ec9f14f4defe0d00999cc0e2
artdevz <prodevarthurdantas@gmail.com>
2025-01-26 13:19:06 -0300

    Footer Impl

--- src/app/auth/login.service.ts
+++ src/app/auth/login.service.ts
@@ -8,12 +8,36 @@ import { Observable } from 'rxjs';
 export class LoginService {
 
   private api = "http://localhost:8080/api/auth/login"
+  private TOKEN = "jwt";
 
   constructor(private http: HttpClient) {}
 
   login(email: string, password: string): Observable<string> {
     const payload = { email, password };
     return this.http.post<string>(this.api, payload, { responseType: "text" as "json" });
+  }
+
+  getToken(): string | null {
+    if (typeof window !== 'undefined' && window.localStorage) return localStorage.getItem(this.TOKEN);
+    return null;
+  }
+
+  getDecodedToken(): any | null {
+    const token = this.getToken();
+    if (!token) return null;
+    try {
+      const payload = token.split('.')[1];
+      return JSON.parse(atob(payload));
+    }
+    catch (error) {
+      console.error("Error at decoded Token");
+      return null;
+    }
+  }
+
+  getLoggedInUser(): any | null {
+    const decodedToken = this.getDecodedToken();
+    return decodedToken ? { id: decodedToken.id, name: decodedToken.name, email: decodedToken.email, password: "" } : null;
   }
 
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user';
+import { CommonModule } from '@angular/common';
+import { LoginService } from '../../auth/login.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  user: User = new User("", "", "", "");
+
+  constructor(private authS: LoginService) {}
+
+  ngOnInit(): void {
+    this.user = this.authS.getLoggedInUser();
+  }
 
 }
